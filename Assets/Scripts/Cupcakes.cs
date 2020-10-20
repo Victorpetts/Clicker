2c80e76cb05b3a57b6b3082279da58008218cff3
Victorpetts <vike.pet@hotmail.com>
2020-10-20 21:48:02 +0200

    saves the current amount of cupcakes when ending the game. Makes the amout added per click configurable

---
+++ Assets/Scripts/Cupcakes.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Cupcakes : MonoBehaviour
+{
+    public Text cupcakeAmountText;
+    public int cupcakesPerClick = 5;
+    
+    private int _cupcakeAmount;
+    
+    public int CupcakeAmount {
+        get => this._cupcakeAmount;
+        set {
+            this._cupcakeAmount = value;
+            this.cupcakeAmountText.text = value.ToString("0 Cupcakes");
+        } 
+    }
+    
+    void Start()
+    {
+        CupcakeAmount = PlayerPrefs.GetInt("Cupcakes", 0);
+    }
+    
+    void OnDestroy() {
+        PlayerPrefs.SetInt("Cupcakes", CupcakeAmount);
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            ProduceCupcakes();
+        }
+    }
+
+    public void ProduceCupcakes()
+    {
+        CupcakeAmount += cupcakesPerClick;
+    }
+}
