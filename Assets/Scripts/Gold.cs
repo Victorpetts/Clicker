08cd6f849072be8032c36f7d265683a065c42464
Victorpetts <vike.pet@hotmail.com>
2020-10-20 20:16:23 +0200

    Adds text with cupcake(gold) amount and button that adds more cupcakes

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Gold : MonoBehaviour
+{
+    public int goldAmount;
+    public Text goldAmountText;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        goldAmountText.text = goldAmount.ToString("You have 0 Cupcakes");
+    }
+
+    public void ProduceGold()
+    {
+        goldAmount += 5;
+    }
+}
