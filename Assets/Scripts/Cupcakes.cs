bb99de48b513af91ea6235f4d178c3ca99c12201
Victorpetts <vike.pet@hotmail.com>
2020-10-20 22:39:23 +0200

    adds cupcake presses

--- Assets/Scripts/Cupcakes.cs
+++ Assets/Scripts/Cupcakes.cs
@@ -5,25 +5,38 @@ using UnityEngine.UI;
 public class Cupcakes : MonoBehaviour
 {
     public Text cupcakeAmountText;
+    public Text pressAmountText;
+    
     public int cupcakesPerClick = 5;
     
+    private int _pressAmount;
     private int _cupcakeAmount;
     
     public int CupcakeAmount {
         get => this._cupcakeAmount;
         set {
-            this._cupcakeAmount = value;
-            this.cupcakeAmountText.text = value.ToString("0 Cupcakes");
+            _cupcakeAmount = value;
+            cupcakeAmountText.text = value.ToString("0 Cupcakes");
+        } 
+    }
+    
+    public int PressAmount {
+        get => this._pressAmount;
+        set {
+            _pressAmount = value;
+            pressAmountText.text = value.ToString("Cupcake presses: 0");
         } 
     }
     
     void Start()
     {
         CupcakeAmount = PlayerPrefs.GetInt("Cupcakes", 0);
+        PressAmount = PlayerPrefs.GetInt("Presses", 0);
     }
     
     void OnDestroy() {
         PlayerPrefs.SetInt("Cupcakes", CupcakeAmount);
+        PlayerPrefs.SetInt("Presses", PressAmount);
     }
     
     void Update()
@@ -36,5 +49,13 @@ public class Cupcakes : MonoBehaviour
     public void ProduceCupcakes()
     {
         CupcakeAmount += cupcakesPerClick;
+    }
+    
+    public void CreatePress()
+    {
+        if (CupcakeAmount >= 100) {
+            CupcakeAmount -= 100;
+            PressAmount++;
+        }
     }
 }
